9d34cc20b5bbd85430177680af0d063450c070b3
Greg Schmitt <gregjschmitt@gmail.com>
2021-12-08 14:17:29 -0500

    assignment completed

--- main.js
+++ main.js
@@ -2,10 +2,40 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+function handleError(flag, errorMessage) {
+  const errorDiv = document.querySelector("div#modal");
 
+  if (flag) {
+    document.getElementById("modal-message").textContent = errorMessage;
+    errorDiv.classList.remove("hiddne");
+  } else {
+    errorDiv.classList.add("hidden");
+  }
+    
+}
 
+const activateHeart = (e) => {
+  const heart = e.target;
 
+  if (heart.className.split(" ").includes("activated-heart")) {
+    heart.innerHTML = EMPTY_HEART;
+    heart.classList.remove("activated-heart");
+  }
+  else {
+    mimicServerCall()
+      .then(message => { 
+        handleError(false);
+        heart.innerHTML = FULL_HEART;
+        heart.classList.add("activated-heart");
+      })
+      .catch (message => handleError(true, message));
+  }
+ 
+}
+
+const hearts = document.querySelectorAll(".like-glyph")
+hearts.forEach(el => el.addEventListener("click", activateHeart));
+handleError(false);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
